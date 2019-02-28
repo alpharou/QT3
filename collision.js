e8c2a462cb16b9f3bb23e37caa2f2412c77d5d43
alpharou <alpharou.dev@gmail.com>
2019-02-28 16:34:11 +0100

    Added simplified vector collision. TODO: Fix wall glitch

--- collision.js
+++ collision.js
@@ -4,39 +4,76 @@ function moveDots(points) {
 	if (typeof points != "object") {return false;}
 	if (typeof points[0] != "object") {return false;}
 	
-	let tempPos;
+	let testPos;
 	
 	for (let i = 0; i < points.length; i++) {
 		
-		tempPos = [points[i].x + points[i].data[0], points[i].y + points[i].data[1]];
-		if (tempPos[0] + points[i].data[2] > gridWidth) {
+		testPos = [points[i].x + points[i].data[0], points[i].y + points[i].data[1]];
+		if (testPos[0] + points[i].data[2] > gridWidth) {
 			
-			deflectDot(points[i], "VERT");
-			return "DEFLECTED VERT";
-			
-		}
-		if (tempPos[0] - points[i].data[2] < 0) {
-			
-			deflectDot(points[i], "VERT");
-			return "DEFLECTED VERT";
+			if (testPos[0] + points[i].data[2] < gridWidth + 5) {
+				deflectDot(points[i], "VERT");
+			} else {
+				points[i].x += points[i].data[0];
+			}
 			
 		}
-		if (tempPos[1] + points[i].data[2] > gridHeight) {	
+		else if (testPos[0] - points[i].data[2] < 0) {
 			
-			deflectDot(points[i], "HORI");
-			return "DEFLECTED HORI";
+			if (testPos[0] - points[i].data[2] > -6) {
+				deflectDot(points[i], "VERT");
+			} else {
+				points[i].x += points[i].data[0];
+			}
 			
 		}
-		if (tempPos[1] - points[i].data[2] < 0) {
+		else if (testPos[1] + points[i].data[2] > gridHeight) {	
 			
-			deflectDot(points[i], "HORI");
-			return "DEFLECTED HORI";
+			if (testPos[1] + points[i].data[2] < gridHeight + 5) {
+				deflectDot(points[i], "HORI");
+			} else {
+				points[i].y += points[i].data[1];
+			}
 			
+		}
+		else if (testPos[1] - points[i].data[2] < 0) {
+			
+			if (testPos[1] - points[i].data[2] > 0 - 5) {
+				deflectDot(points[i], "HORI");
+			} else {
+				points[i].y += points[i].data[1];
+			}
+			
+			
+		} else {
+			
+			points[i].x = testPos[0];
+			points[i].y = testPos[1];
 		}
 		
+		let nearPoints = qt.queryPoints(new Circ(points[i].x, points[i].y, points[i].data[2] + maxSize));
 		
-		points[i].x = tempPos[0];
-		points[i].y = tempPos[1];
+		for (let j = 0; j < nearPoints.length; j++) {
+			
+			if (new Circ(points[i].x, points[i].y, points[i].data[2] - 6).intersects(new Circ(nearPoints[j].x, nearPoints[j].y, nearPoints[j].data[2])) && points[i] != nearPoints[j]) {
+				
+				points[i].x -= points[i].data[0];
+				points[i].y -= points[i].data[1];
+				nearPoints[j].x -= nearPoints[j].data[0];
+				nearPoints[j].y -= nearPoints[j].data[1];
+				
+			}
+			 else if (new Circ(points[i].x, points[i].y, points[i].data[2]).intersects(new Circ(nearPoints[j].x, nearPoints[j].y, nearPoints[j].data[2])) && points[i] != nearPoints[j]) {
+				
+				bounceDots(qt.points[i], nearPoints[j]);
+				points[i].x += points[i].data[0];
+				points[i].y += points[i].data[1];
+				nearPoints[j].x += nearPoints[j].data[0];
+				nearPoints[j].y += nearPoints[j].data[1];
+				
+			}
+			
+		}
  		
 	}
 	
@@ -64,4 +101,39 @@ function deflectDot(a, direction) {
 	}
 	
 	return false;
+}
+
+function bounceDots(a, b) {
+	
+	/*
+	STEP 1: Get vector that joins the two points and rotate velocity vectors to accomodate for that (so that vectorAB is aligned to X axis)
+	
+	STEP 2: Collide velocities in X axis.
+	
+	STEP 3: Rotate back.
+	*/
+	let v1 = createVector(a.data[0], a.data[1]);
+	let v2 = createVector(b.data[0], b.data[1]);
+	let joint = getVect(a, b);
+	let angle = joint.heading();
+	v1.rotate(-angle);
+	v2.rotate(-angle);
+	
+	//TEMP X collision
+	let tempX = v1.x;
+	v1.set(v2.x, v1.y);
+	v2.set(tempX, v2.y);
+	
+	v1.rotate(angle);
+	v2.rotate(angle);
+	
+	a.data = [v1.x, v1.y, a.data[2]];
+	b.data = [v2.x, v2.y, b.data[2]];
+	
+}
+
+function getVect(a, b) {
+	
+	return createVector(b.x - a.x, b.y - a.y);
+	
 }
--- interface.js
+++ interface.js
@@ -1,10 +1,10 @@
 function setup() {
 
-	//General variables
+	//Interface variables
 	drawQuadrants = true;
 	quadStroke = 1;
-	minSize = 20; //radius, points in the grid
-	maxSize = 70;
+	minSize = 20; //radius by points in the grid
+	maxSize = 100;
 	qtMargin = 30;
 	
 	wW = max(windowWidth, 150);
@@ -16,11 +16,11 @@ function setup() {
 	gridWidth = 2048;
 	gridHeight = 2048;
 	
-	qt = new QuadTree(0, 0, gridWidth, gridHeight, 3);
+	qt = new QuadTree(0, 0, gridWidth, gridHeight, 4);
 	
 	//CursorZone
 	zoney = new Circ(mapMouse()[0], mapMouse()[1], 150);
-	deltaSize = 20; //Amount to resize the cursorZone
+	deltaSize = 30; //Amount to resize the cursorZone
 	nearQuads = [];
 	nearPoints = [];
 
@@ -76,6 +76,34 @@ function draw() {
 		}
 		
 	}
+	
+	//Draw data[0] Quads
+	if (drawQuadrants && qt.points.length > 0) {
+		
+		nearQuads = qt.query(new Circ(qt.points[0].x, qt.points[0].y, qt.points[0].data[2] + maxSize));
+
+		for (let q of nearQuads) {
+
+			fill(0);
+			mapped = mapRect(q.r);
+			rect(mapped[0], mapped[1], mapped[2], mapped[3]);
+			fill(220);
+			rect(mapped[0] + quadStroke/2, mapped[1] + quadStroke/2, mapped[2] - quadStroke, mapped[3] - quadStroke);
+
+		}
+		
+	}
+	
+	//Draw data[0] zone
+	if (qt.points.length > 0) {
+		
+		mapped = mapEllipse(new Circ(qt.points[0].x, qt.points[0].y, qt.points[0].data[2] + maxSize));
+		noFill();
+		stroke(255, 0, 255);
+		strokeWeight(3);
+		ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+		
+	}
 
 	//Draw cursorZone
 	mapped = mapEllipse(zoney);
@@ -111,6 +139,92 @@ function draw() {
 		
 	}
 	
+	//draw any two touching
+	if (qt.points.length > 0) {
+		
+		for (let i = 0; i < qt.points.length; i++) {
+			
+			nearPoints = qt.queryPoints(new Circ(qt.points[i].x, qt.points[i].y, qt.points[i].data[2] + maxSize));
+			fill(255, 0, 0);
+			strokeWeight(1);
+			stroke(255, 0, 0);
+			
+			let c1 = new Circ(qt.points[i].x, qt.points[i].y, qt.points[i].data[2]);
+		
+			for (let j = 0; j < nearPoints.length; j++) {
+			
+				let c2 = new Circ(nearPoints[j].x, nearPoints[j].y, nearPoints[j].data[2]);
+			
+				if (qt.points[i] != nearPoints[j] && c1.intersects(c2)) {
+				
+					mapped = mapEllipse(c2);
+					ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+					mapped = mapEllipse(c1);
+					ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+				
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	//Draw near and touching data[0]
+	if (qt.points.length > 0) {
+		nearPoints = qt.queryPoints(new Circ(qt.points[0].x, qt.points[0].y, qt.points[0].data[2] + maxSize));
+		fill(255, 0, 255);
+		strokeWeight(1);
+		stroke(255, 0, 255);
+		if (nearPoints.length > 0) {
+		
+			for (let p of nearPoints) {
+				mapped = mapEllipse(new Circ(p.x, p.y, p.data[2]));
+				ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+			}
+		
+		}
+		
+		fill(255, 0, 0);
+		strokeWeight(1);
+		stroke(255, 0, 0);
+		let c1 = new Circ(qt.points[0].x, qt.points[0].y, qt.points[0].data[2]);
+		
+		for (let p of nearPoints) {
+			
+			let c2 = new Circ(p.x, p.y, p.data[2]);
+			
+			if (p != qt.points[0] && c1.intersects(c2)) {
+				
+				mapped = mapEllipse(c2);
+				ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+				mapped = mapEllipse(c1);
+				ellipse(mapped[0], mapped[1], mapped[2], mapped[3]);
+				
+			}
+				
+		}
+	
+	}
+	
+	//TEMP TEST VECTORE
+	/*
+	createVector(qt.points[0].data[0], qt.points[0].data[1]).angleBetween(createVector(qt.points[1].data[0], qt.points[1].data[1])) * 360 / (2*PI);
+	*/
+	/*
+	if (qt.points.length > 1) {
+		
+		mapped1 = mapPnt(qt.points[0]);
+		mapped2 = mapPnt(new Pnt(qt.points[0].x + qt.points[0].data[0] * 50, qt.points[0].y + qt.points[0].data[1] * 50, qt.points[0].data));
+		line(mapped1[0], mapped1[1], mapped2[0], mapped2[1]);
+		mapped1 = mapPnt(qt.points[1]);
+		mapped2 = mapPnt(new Pnt(qt.points[1].x + qt.points[1].data[0] * 50, qt.points[1].y + qt.points[1].data[1] * 50, qt.points[1].data));
+		line(mapped1[0], mapped1[1], mapped2[0], mapped2[1]);
+		
+	}
+	*/
+	
+	
 	//UPDATE
 	moveDots(qt.points);
 	qt.build();
@@ -144,6 +258,8 @@ function mouseWheel(event) {
   
 }
 
+//Mappers for gridshapes
+
 function mapRect(r) {
 	
 	let x = map(r.x, 0, gridWidth, mW, wW - mW, true);
@@ -157,8 +273,8 @@ function mapRect(r) {
 
 function mapPnt(p) {
 	
-	let x = map(p.x, 0, gridWidth, mW, wW - mW, true);
-	let y = map(p.y, 0, gridHeight, mH, wH - mH, true);
+	let x = map(p.x, 0, gridWidth, mW, wW - mW);
+	let y = map(p.y, 0, gridHeight, mH, wH - mH);
 	
 	return [x, y];
 	
@@ -168,8 +284,8 @@ function mapEllipse(c) {
 	
 	let x = map(c.x, 0, gridWidth, mW, wW - mW);
 	let y = map(c.y, 0, gridHeight, mH, wH - mH);
-	let w = map(c.r*2, 0, gridWidth, 0, wW - mW*2, true);
-	let h = map(c.r*2, 0, gridHeight, 0, wH - mH*2, true);
+	let w = map(c.r*2, 0, gridWidth, 0, wW - mW*2);
+	let h = map(c.r*2, 0, gridHeight, 0, wH - mH*2);
 	
 	return [x, y, w, h];
 	
